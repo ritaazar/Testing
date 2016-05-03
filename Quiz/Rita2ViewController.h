1fc06b3fe844f6dad0ed8891a56210c8cfeedc30
ritaazar <rita.azar0@gmail.com>
2016-05-03 14:36:14 +0300

    Commit rita2

---
+++ Quiz/Rita2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Rita2ViewController.h
+//  Quiz
+//
+//  Created by Azar, Rita on 5/3/16.
+//  Copyright © 2016 Azar, Rita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Rita2ViewController : UIViewController
+
+@end
