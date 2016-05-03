69055002e5537febdab7407e465e7defec8c9db2
royrhayem <royrhayem@gmail.com>
2016-05-03 14:36:22 +0300

    committing Roy2

---
+++ Quiz/Roy2.h
@@ -0,0 +1,13 @@
+//
+//  Roy2.h
+//  Quiz
+//
+//  Created by Roy Rhayem on 5/3/16.
+//  Copyright © 2016 Azar, Rita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Roy2 : UIViewController
+
+@end
---
+++ Quiz/Roy2.m
@@ -0,0 +1,37 @@
+//
+//  Roy2.m
+//  Quiz
+//
+//  Created by Roy Rhayem on 5/3/16.
+//  Copyright © 2016 Azar, Rita. All rights reserved.
+//
+
+#import "Roy2.h"
+
+@interface Roy2 ()
+
+@end
+
+@implementation Roy2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
